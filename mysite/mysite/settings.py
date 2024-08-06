06b915bd667e08fe8c123d61b01ac7b62ea2ddb9
BrunoBReis <brunobragancadosreis@gmail.com>
2024-08-06 11:18:13 -0300

    deploy: moving secret key back

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+# My imports
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-kqgz_8pb!hrqn-r&!1zp^@)iy4!9=!btr-pr$*=-_%dm)5wehw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
